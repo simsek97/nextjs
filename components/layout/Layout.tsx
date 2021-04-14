ec30d9756ae73541106f1e2edd38846a9a64d87d
Adrian Gallardo <adrian.gallardo@cbsinteractive.com>
2021-04-14 15:28:34 -0400

    Adding header (wip)

--- components/layout/Layout.tsx
+++ components/layout/Layout.tsx
@@ -1,15 +1,22 @@
 import React from "react";
+import dynamic from 'next/dynamic'
 import Footer from "./Footer";
 
 type TLayoutProps = {
   children: React.ReactNode;
 };
 
+const DynamicHeaderWithNoSSR = dynamic(
+  () => import('./Header'),
+  { ssr: false }
+)
+
 const Layout: React.FC<TLayoutProps> = (props) => {
   const { children } = props;
 
   return (
     <div className="layout">
+      <DynamicHeaderWithNoSSR />
       {children}
       <Footer />
     </div>
