d2f6f90529be64069bc714e50918354006d6df99
David <jdferreira2429@gmail.com>
2021-04-15 10:55:53 -0400

    Added viewport meta tag for mobile devices

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,6 +7,7 @@ export default function Home() {
       <Head>
         <title>Create Next App</title>
         <link rel='icon' href='/favicon.ico' />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
       </Head>
 
       <main className={styles.main}>
