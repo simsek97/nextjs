ebbfec277181609344334ae21c86b378eab039a4
David <jdferreira2429@gmail.com>
2021-04-15 10:29:23 -0400

    added logo title and sponsor to marquee

--- components/layout/Marquee.tsx
+++ components/layout/Marquee.tsx
@@ -1,7 +1,27 @@
 import React from 'react';
+import styles from '../../styles/Marquee.module.css';
+
 
 const Marquee: React.FC = () => {
-  return <h1>this is the future Marquee</h1>;
-};
+  return (
+      <div className={styles.container}>
+        <div className={styles.pageTitle}>
+            <div className={styles.logo}>
+                <img alt="team logo" src="https://sportsfly.cbsistatic.com/fly-240/bundles/sportsmediacss/images/league-logos/light/CHLG.svg"/>
+            </div>
+            <div>
+                <h1>UEFA Champions League Scores</h1>
+                <aside>
+                    Scores for Apr 14, 2021
+                </aside>
+            </div>
+        </div>
+        <div className={styles.sponsor}>               
+            <figure>
+                <img src="https://sportsfly.cbsistatic.com/fly-240/bundles/sportsmediacss/images/partners/william-hill/odds-by-vertical-stacked.svg" />
+            </figure>
+        </div>
+      </div>
+ )};
 
 export default Marquee;
