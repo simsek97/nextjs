a500bfcf122e3d2289a180629c2771e6c0b6132b
Simsek Mert <simsek97@gmail.com>
2021-04-15 18:34:45 -0400

    styling updates

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,7 +8,10 @@ export default function Home() {
         <title>
           CBS Sports - News, Live Scores, Schedules, Fantasy Games, Video and more. - CBSSports.com
         </title>
-        <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="icon"
+          href="https://sportsfly.cbsistatic.com/bundles/sportsmediacss/images/core/cbssports-app-icon.svg"
+        />
         <meta
           name="viewport"
           content="width=device-width, initial-scale=1, maximum-scale=1, minimum-scale=1, user-scalable=0"
