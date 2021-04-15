f09c2267bf53955ced0b8e631e4a182bd24a1650
David <jdferreira2429@gmail.com>
2021-04-15 12:32:33 -0400

    correct meta value for mobile and mobile marquee

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,7 @@ export default function Home() {
       <Head>
         <title>Create Next App</title>
         <link rel='icon' href='/favicon.ico' />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, minimum-scale=1, user-scalable=0" />
       </Head>
 
       <main className={styles.main}>
